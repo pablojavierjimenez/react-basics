770ff8b546d76c570699805b4e2e4dd8716375a8
Pablo Javier Jimenez <pablojavierjimenez@gmail.com>
2019-02-21 16:55:27 -0300

    finish of videotutorial where we learn about this.props, PropTypes, and this.props.children

--- src/App.js
+++ src/App.js
@@ -7,17 +7,36 @@ import Header from './components/Header';
 
 class App extends Component {
   render() {
+    let textFromApp = 'como este que viene directo del componente app.js';
     let user = {
       name: 'jey jey',
-      hobbies: [ 'running', 'play violin']
+      hobbies: ['running', 'play violin']
     };
     return (
       <div className="App">
         <Header />
-        <Home name={"Chaboncito"} age={27} user={user}/>
+        {/**
+         * _Acerca de lo elemento hijos de un componente_
+         * los componentes pueden utilizarse como un cierre en si mismos,
+         * eg: *<Home />*
+         * o tambien como elementos tag de apertura y cierre, los cuales pueden incluir 
+         * otros elementos html, o componentes 
+         * estos seran recibidos en en la clase en el objeto _this.props.children_
+         * eg: 
+         *    *<Home>*
+         *      *<p>soy hijo de home</p>*
+         *    *</Home>*
+        */}
+        <Home name={"Chaboncito"} age={34} user={user} >
+          {/* you most put a child element cause isRequired */}
+          <p>
+            y aca pondria otro tipo de textos
+            <mark> {textFromApp} </mark>
+          </p>
+        </Home>
       </div>
-      );
-    }
+    );
   }
+}
 
-  export default App;
+export default App;
