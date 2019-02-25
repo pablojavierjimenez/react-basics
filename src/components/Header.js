ea5af5ccc2444cba384a909cd528de8645b6ea92
Pablo Javier Jimenez <pablojavierjimenez@gmail.com>
2019-02-25 19:19:27 -0300

    change header to an stateless component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 
 
-class Header extends Component {
-    render() {
-    return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <nav className="App">
-          <ul>
-            <li><a href="./home">HOME</a></li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-}
+/**
+ * STATELESS component
+ * in this kind of component the principal difference is
+ * that they
+ * class Header extends Component {
+ */
+const Header = ( props ) => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <nav className="App">
+        <ul>
+          <li><a href="./home">{props.homeLink}</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
