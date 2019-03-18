8f53d95b99edb4e09ae88e9298edd4f527508c05
Pablo Javier Jimenez <pablojavierjimenez@gmail.com>
2019-03-18 17:56:08 -0300

    update Header.js of video N° 13

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,34 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 
 
-class Header extends Component {
-    render() {
-    return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <nav className="App">
-          <ul>
-            <li><a href="./home">HOME</a></li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-}
+/**
+ * STATELESS component
+ * in this kind of component the principal difference is
+ * that insted of extend from _React Component_
+ * its directly expose a function.
+ * en este tipo de componentes podemos usar las props directamente,
+ * porque al ser solo una funcion las variables recibidas directamente por parametros
+ * se combierten en variables dentro del scope
+ * @example
+ * const Header = ( props ) => {
+ *   return (...)
+ * };
+ * *insted
+ * class Header extends Component {
+ *  ...
+ * };
+ */
+const Header = ( props ) => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <nav className="App">
+        <ul>
+          <li><a href="./home">{props.homeLink}</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
