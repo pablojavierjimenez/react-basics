f0b71f7b8dfff387236206f7ab1391a6e39a3003
Pablo Javier Jimenez <pablojavierjimenez@gmail.com>
2019-03-18 12:36:05 -0300

    final update for App.js on the video n°6

--- src/App.js
+++ src/App.js
@@ -6,15 +6,35 @@ import Home from './components/Home';
 import Header from './components/Header';
 
 class App extends Component {
+  // METHODS:
+  onGreet() {
+    alert('que hacelga!');
+  }
+  onCHangeLinkName() {
+    alert('que hacelga!');
+  }
   render() {
-    let textFromApp = 'como este que viene directo del componente app.js';
+    /** 
+     * Como veras lo que declaro aca adentro es parte de la funcion _render()_
+     * es por eso que puedo accesarlo libremente sin la necesidad de apelar al _this_
+    */
+    let textFromApp = ' como este que viene directo del componente app.js ';
     let user = {
       name: 'jey jey',
       hobbies: ['running', 'play violin']
     };
+    /**
+     * 
+     */
     return (
       <div className="App">
-        <Header />
+        {/**
+         * **Header** is an example of an stateLess component
+         * its mean that the component itself n use or implement the 
+         * react state, insted of that it use the values recived by props
+         * @reference https://reactjs.org/docs/state-and-lifecycle.html#the-data-flows-down
+         */}
+        <Header homeLink="Home" />
         {/**
          * _Acerca de lo elemento hijos de un componente_
          * los componentes pueden utilizarse como un cierre en si mismos,
@@ -26,14 +46,11 @@ class App extends Component {
          *    *<Home>*
          *      *<p>soy hijo de home</p>*
          *    *</Home>*
-        */}
-        <Home name={"Chaboncito"} age={34} user={user} >
-          {/* you most put a child element cause isRequired */}
-          <p>
-            y aca pondria otro tipo de textos
-            <mark> {textFromApp} </mark>
-          </p>
+         */}
+        <Home name={"Chaboncito"} initialAge={ 34 } user={ user } greet={ this.onGreet }>
+          <mark> {textFromApp} </mark>
         </Home>
+
       </div>
     );
   }
